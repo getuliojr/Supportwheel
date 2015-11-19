10f6e8d246a899b635f99bceb720bab95aa247b6
Getulio Romão Campos Junior <gromao@gmail.com>
2015-11-19 15:29:14 -0200

    apos instalar o bower, referencia ele dentro do bin do node_modules

--- Code/Crossover.Frontend/bowerbatch.bat
+++ Code/Crossover.Frontend/bowerbatch.bat
@@ -3,7 +3,7 @@ echo Instalando Bower
 call npm install bower
 echo
 echo Limpando Cache Bower
-call bower cache clean
+call node_modules\.bin\bower cache clean
 echo
 echo Instalando Bower.json
-call bower install
+call node_modules\.bin\bower install
