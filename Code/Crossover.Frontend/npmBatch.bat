e2b8596594d6c8d809d450f601fa35627f79403a
Getulio Romão Campos Junior <gromao@gmail.com>
2015-11-19 16:21:38 -0200

    tentando de novo, instalou arquivos devDependencies e não devia. Esqueci da flag --production

--- Code/Crossover.Frontend/npmBatch.bat
+++ Code/Crossover.Frontend/npmBatch.bat
@@ -1,6 +1,6 @@
 echo
 echo Instalando pacotes através do NPM
-call npm install 
+call npm install --production
 echo
 echo Copiando scripts para a pasta correta
 call xcopy node_modules .\app\libs /y /i /s
