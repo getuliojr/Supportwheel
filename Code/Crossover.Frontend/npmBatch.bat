6e5e38abe74365a70b8303fb6d496173900cb434
Getulio Romão Campos Junior <gromao@gmail.com>
2015-11-19 16:33:32 -0200

    copiou, mas parece que para o lugar errado. tentando de novo para ver o caminho completo

--- Code/Crossover.Frontend/npmBatch.bat
+++ Code/Crossover.Frontend/npmBatch.bat
@@ -3,6 +3,6 @@ echo Instalando pacotes através do NPM
 call npm install --production
 echo
 echo Copiando scripts para a pasta correta
-call xcopy node_modules .\app\libs /y /i /s
+call xcopy node_modules app\libs /y /i /s /e /f
 echo
 echo Instalação das bibliotecas concluídas
