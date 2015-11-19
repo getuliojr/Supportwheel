fc218c80c84c10429496a3377ef73248aab1cbc9
Getulio Romão Campos Junior <gromao@gmail.com>
2015-11-19 19:18:10 -0200

    build

---
+++ Code/Crossover.DependencyResolver/Migrations/201511192116174_InitialMigrations1.cs
@@ -0,0 +1,16 @@
+namespace Crossover.DependencyResolver.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigrations1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
